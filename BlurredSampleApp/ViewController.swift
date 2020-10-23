403eacbd9ae02798e8dc3b3d0c548efec94163e8
Hamid <mail.hamid.hoseini@gmail.com>
2020-10-22 23:32:56 -0700

    first commit

--- BlurredSampleApp/ViewController.swift
+++ BlurredSampleApp/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let imageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "background"))
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.addSubview(imageView)
+        imageView.frame = view.bounds
+        
+        createBlur()
     }
 
-
+    func createBlur() {
+        let blureEffect = UIBlurEffect(style: .dark)
+        let visualEffectView = UIVisualEffectView(effect: blureEffect)
+        visualEffectView.frame = view.bounds
+        visualEffectView.alpha = 0
+        view.addSubview(visualEffectView)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now()+2) {
+            UIView.animate(withDuration: 0.5) {
+                visualEffectView.alpha = 1
+            }
+        }
+    }
 }
 
